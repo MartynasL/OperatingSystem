b354d0fac39630c9e054fc37949e341eed32409a
Martynas <martynas.lycius@gmail.com>
2014-05-19 11:18:45 +0300

    Created ProcessDescriptor and added Process OSCore RegState and Resource classes

---
+++ OperatingSystem/OSCore.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatingSystem
+{
+    public class OSCore
+    {
+        public enum ProcessName
+        {
+            START_STOP, JOB_INPUT, JOB_LOADER, MAIN_PROC, PRINT_LINE,
+            INPUT_LINE, INTERRUPT, JOB_GOVERNOR, VIRTUAL_MACHINE
+        }
+
+        public enum ProcessState
+        {
+            RUN, READY, BLOCKED_STOPPED, READY_STOPPED, BLOCKED
+        }
+
+        public enum ResourceName
+        {
+            TRECIAS_KANALAS, SUPERVIZORINE_ATMINTIS, VARTOTOJO_ATMINTIS,
+            PIRMAS_KANALAS, ANTRAS_KANALAS, PABAIGA, UZDUOTIS_ISORINEJE_ATMINTYJE,
+            UZDUOTIS_VARTOTOJO_ATMINTYJE, IVYKO_PERTRAUKIMAS, EILUTE_IVESTA, EILUTE_ATSPAUSDINTA,
+            PRANESIMAS_PEOCESUI_INPUT_LINE, PRANESIMAS_PROCESUI_PRINT_LINE, PERTRAUKIMAS,
+            UZDUOTIES_IVEDIMAS
+        }
+    }
+}
---
+++ OperatingSystem/ProcessDescriptor.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatingSystem
+{
+    public class ProcessDescriptor
+    {
+        public LinkedList<Process> processList;
+        public int ID;
+        public OSCore.ProcessName externalID;
+        public RegState savedState;
+        public VirtualRealMachine.CPU processor;
+        public OSCore os;
+        public LinkedList<Resource> createdResList;
+        public LinkedList<Resource> ownedResList;
+        public LinkedList<OSCore.ResourceName> waitingResList;
+        public OSCore.ProcessState state;
+        public Process parent;
+        public LinkedList<Process> childrenList;
+        public int priority;
+
+        public ProcessDescriptor(LinkedList<Process> processList,
+                                 int ID, OSCore.ProcessName externalID,
+                                 RegState savedState, VirtualRealMachine.CPU processor,
+                                 OSCore os, OSCore.ProcessState state, Process parent,
+                                 int priority)
+        {
+            this.processList = processList;
+            this.ID = ID;
+            this.savedState = savedState;
+            this.processor = processor;
+            this.os = os;
+            this.state = state;
+            this.parent = parent;
+            this.priority = priority;
+
+            childrenList = new LinkedList<Process>();
+            createdResList = new LinkedList<Resource>();
+            ownedResList = new LinkedList<Resource>();
+            waitingResList = new LinkedList<OSCore.ResourceName>();
+        }
+    }
+}
