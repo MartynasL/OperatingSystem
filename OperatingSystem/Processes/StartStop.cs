b744ffb9751d0e9a71888bb783aae396796a253f
Martynas <martynas.lycius@gmail.com>
2014-05-21 20:55:50 +0300

    Implemented StartStop

--- OperatingSystem/Processes/StartStop.cs
+++ OperatingSystem/Processes/StartStop.cs
@@ -73,12 +73,13 @@ namespace OperatingSystem.Processes
 
         private void createStaticResources()
         {
-            //descriptor.os.createResource(this, OSCore.ResourceName.TRECIAS_KANALAS);
-            //descriptor.os.createResource(this, OSCore.ResourceName.SUPERVIZORINE_ATMINTIS);
-            //descriptor.os.createResource(this, OSCore.ResourceName.VARTOTOJO_ATMINTIS);
-            //descriptor.os.createResource(this, OSCore.ResourceName.PIRMAS_KANALAS);
-            //descriptor.os.createResource(this, OSCore.ResourceName.ANTRAS_KANALAS);
-            //Parasyt koks komponentas pridedamas
+            VirtualRealMachine.Machine machine = descriptor.os.machine;
+
+            descriptor.os.createResource(this, OSCore.ResourceName.TRECIAS_KANALAS, machine.cpu);
+            descriptor.os.createResource(this, OSCore.ResourceName.SUPERVIZORINE_ATMINTIS, machine.supervisorMemory);
+            descriptor.os.createResource(this, OSCore.ResourceName.VARTOTOJO_ATMINTIS, machine.memory);
+            descriptor.os.createResource(this, OSCore.ResourceName.PIRMAS_KANALAS, machine.cpu);
+            descriptor.os.createResource(this, OSCore.ResourceName.ANTRAS_KANALAS, machine.cpu);
         }
 
     }
